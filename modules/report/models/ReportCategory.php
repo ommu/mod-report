f4edc6d6821a663883c8b506c0b2392f8cd480e0
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-05-12 12:14:40 +0700

    merge with development source

--- modules/report/models/ReportCategory.php
+++ modules/report/models/ReportCategory.php
@@ -321,23 +321,23 @@ class ReportCategory extends CActiveRecord
 				$location = strtolower(Yii::app()->controller->module->id.'/'.Yii::app()->controller->id);
 				$title=new OmmuSystemPhrase;
 				$title->location = $location.'_title';
-				$title->en = $this->title;
+				$title->en_us = $this->title;
 				if($title->save())
 					$this->name = $title->phrase_id;
 
 				$desc=new OmmuSystemPhrase;
 				$desc->location = $location.'_description';
-				$desc->en = $this->description;
+				$desc->en_us = $this->description;
 				if($desc->save())
 					$this->desc = $desc->phrase_id;
 				
 			} else {
 				$title = OmmuSystemPhrase::model()->findByPk($this->name);
-				$title->en = $this->title;
+				$title->en_us = $this->title;
 				$title->save();
 
 				$desc = OmmuSystemPhrase::model()->findByPk($this->desc);
-				$desc->en = $this->description;
+				$desc->en_us = $this->description;
 				$desc->save();
 			}
 		}
