9fd6ddac05564033263c96d6f02f56209e751d8a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-08-23 23:05:16 +0700

    comment: add category in detail

--- modules/report/views/o/comment/_form.php
+++ modules/report/views/o/comment/_form.php
@@ -20,95 +20,49 @@
 	'enableAjaxValidation'=>true,
 	//'htmlOptions' => array('enctype' => 'multipart/form-data')
 )); ?>
-
-<?php //begin.Messages ?>
-<div id="ajax-message">
-	<?php echo $form->errorSummary($model); ?>
-</div>
-<?php //begin.Messages ?>
-
-<fieldset>
-
-	<div class="clearfix publish">
-		<?php echo $form->labelEx($model,'publish'); ?>
-		<div class="desc">
-			<?php echo $form->checkBox($model,'publish'); ?>
-			<?php echo $form->labelEx($model,'publish'); ?>
-			<?php echo $form->error($model,'publish'); ?>
-			<?php /*<div class="small-px silent"></div>*/?>
-		</div>
-	</div>
-
-	<div class="clearfix">
-		<?php echo $form->labelEx($model,'report_id'); ?>
-		<div class="desc">
-			<?php echo $form->textField($model,'report_id',array('size'=>11,'maxlength'=>11)); ?>
-			<?php echo $form->error($model,'report_id'); ?>
-			<?php /*<div class="small-px silent"></div>*/?>
-		</div>
-	</div>
-
-	<div class="clearfix">
-		<?php echo $form->labelEx($model,'user_id'); ?>
-		<div class="desc">
-			<?php echo $form->textField($model,'user_id',array('size'=>11,'maxlength'=>11)); ?>
-			<?php echo $form->error($model,'user_id'); ?>
-			<?php /*<div class="small-px silent"></div>*/?>
-		</div>
-	</div>
-
-	<div class="clearfix">
-		<?php echo $form->labelEx($model,'comment_text'); ?>
-		<div class="desc">
-			<?php echo $form->textArea($model,'comment_text',array('rows'=>6, 'cols'=>50)); ?>
-			<?php echo $form->error($model,'comment_text'); ?>
-			<?php /*<div class="small-px silent"></div>*/?>
-		</div>
-	</div>
-
-	<div class="clearfix">
-		<?php echo $form->labelEx($model,'creation_date'); ?>
-		<div class="desc">
-			<?php echo $form->textField($model,'creation_date'); ?>
-			<?php echo $form->error($model,'creation_date'); ?>
-			<?php /*<div class="small-px silent"></div>*/?>
-		</div>
-	</div>
-
-	<div class="clearfix">
-		<?php echo $form->labelEx($model,'modified_date'); ?>
-		<div class="desc">
-			<?php echo $form->textField($model,'modified_date'); ?>
-			<?php echo $form->error($model,'modified_date'); ?>
-			<?php /*<div class="small-px silent"></div>*/?>
-		</div>
-	</div>
-
-	<div class="clearfix">
-		<?php echo $form->labelEx($model,'modified_id'); ?>
-		<div class="desc">
-			<?php echo $form->textField($model,'modified_id',array('size'=>11,'maxlength'=>11)); ?>
-			<?php echo $form->error($model,'modified_id'); ?>
-			<?php /*<div class="small-px silent"></div>*/?>
-		</div>
-	</div>
-
-	<div class="submit clearfix">
-		<label>&nbsp;</label>
-		<div class="desc">
-			<?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('phrase', 'Create') : Yii::t('phrase', 'Save'), array('onclick' => 'setEnableSave()')); ?>
-		</div>
-	</div>
-
-</fieldset>
-<?php /*
 <div class="dialog-content">
+	<fieldset>
+
+		<?php //begin.Messages ?>
+		<div id="ajax-message">
+			<?php echo $form->errorSummary($model); ?>
+		</div>
+		<?php //begin.Messages ?>
+
+		<div class="clearfix">
+			<?php echo $form->labelEx($model,'report_id'); ?>
+			<div class="desc">
+				<?php echo $form->textField($model,'report_id',array('size'=>11,'maxlength'=>11)); ?>
+				<?php echo $form->error($model,'report_id'); ?>
+				<?php /*<div class="small-px silent"></div>*/?>
+			</div>
+		</div>
+
+		<div class="clearfix">
+			<?php echo $form->labelEx($model,'comment_text'); ?>
+			<div class="desc">
+				<?php echo $form->textArea($model,'comment_text',array('rows'=>6, 'cols'=>50, 'slass'=>'span-11')); ?>
+				<?php echo $form->error($model,'comment_text'); ?>
+				<?php /*<div class="small-px silent"></div>*/?>
+			</div>
+		</div>
+
+		<div class="clearfix publish">
+			<?php echo $form->labelEx($model,'publish'); ?>
+			<div class="desc">
+				<?php echo $form->checkBox($model,'publish'); ?>
+				<?php echo $form->labelEx($model,'publish'); ?>
+				<?php echo $form->error($model,'publish'); ?>
+				<?php /*<div class="small-px silent"></div>*/?>
+			</div>
+		</div>
+
+	</fieldset>
 </div>
 <div class="dialog-submit">
 	<?php echo CHtml::submitButton($model->isNewRecord ? Yii::t('phrase', 'Create') : Yii::t('phrase', 'Save') ,array('onclick' => 'setEnableSave()')); ?>
 	<?php echo CHtml::button(Yii::t('phrase', 'Cancel'), array('id'=>'closed')); ?>
 </div>
-*/?>
 <?php $this->endWidget(); ?>
 
 
--- modules/report/views/o/comment/_view_report.php
+++ modules/report/views/o/comment/_view_report.php
@@ -13,13 +13,15 @@
  *
  */
 
+	$category_name = $model->report->cat->name ? Phrase::trans($model->report->cat->name) : '-';
 	$report_url = $model->report->report_url ? $model->report->report_url : '-';
 	$report_body = $model->report->report_body ? $model->report->report_body : '-';
 	$report_date = $model->report->report_date ? Utility::dateFormat($model->report->report_date, true) : '-';
 ?>
 
 <ul>
-	<li><?php echo Yii::t('phrase', 'Name: $report_url', array('$report_url'=>$report_url));?></li>
-	<li><?php echo Yii::t('phrase', 'Email: $report_body', array('$report_body'=>$report_body));?></li>
-	<li><?php echo Yii::t('phrase', 'Phone: $report_date', array('$report_date'=>$report_date));?></li>
+	<li><?php echo Yii::t('phrase', 'Category: $category_name', array('$category_name'=>$category_name));?></li>
+	<li><?php echo Yii::t('phrase', 'URL: $report_url', array('$report_url'=>$report_url));?></li>
+	<li><?php echo Yii::t('phrase', 'Error: $report_body', array('$report_body'=>$report_body));?></li>
+	<li><?php echo Yii::t('phrase', 'Date: $report_date', array('$report_date'=>$report_date));?></li>
 <ul>
