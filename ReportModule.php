96607fe7576ea2c177f09752fbcad188bda7837b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-10-23 20:52:33 +0700

    add model view directory

--- ReportModule.php
+++ ReportModule.php
@@ -27,6 +27,7 @@ class ReportModule extends CWebModule
 		// import the module-level models and components
 		$this->setImport(array(
 			'report.models.*',
+			'report.models.view.*',
 			'report.components.*',
 		));
 	}
