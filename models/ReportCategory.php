4961c68da82bed701f971f0524e106a1c62f7c0b
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2017-11-07 11:20:41 +0700

    change sluggable extensions

--- models/ReportCategory.php
+++ models/ReportCategory.php
@@ -55,7 +55,7 @@ class ReportCategory extends CActiveRecord
 	{
 		return array(
 			'sluggable' => array(
-				'class'=>'application.vendor.mintao.yii-behavior-sluggable.SluggableBehavior',
+				'class'=>'ext.yii-behavior-sluggable.SluggableBehavior',
 				'columns' => array('title.en_us'),
 				'unique' => true,
 				'update' => true,
