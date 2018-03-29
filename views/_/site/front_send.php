2d7ca14d62ca1b8150625e270fd7bcb1e4385485
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-03-29 11:48:00 +0700

    commit

--- views/_/site/front_send.php
+++ views/_/site/front_send.php
@@ -18,7 +18,7 @@
 	);
 ?>
 
-<?php if(isset($_GET['type']) && $_GET['type'] == 'success') {?>	
+<?php if(isset($_GET['type']) && $_GET['type'] == 'success') {?>
 	<div class="dialog-content">
 		Report send success
 	</div>
