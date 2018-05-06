482a8abcb701d621bdc7f987fe9943cc1937a80c
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-05-06 20:17:22 +0700

    commit

--- views/comment/_form.php
+++ views/comment/_form.php
@@ -36,24 +36,24 @@ $redactorOptions = [
 	],
 ]); ?>
 
-<!-- <?php echo $form->field($model, 'report_id', ['template' => '{label}<div class="col-md-6 col-sm-6 col-xs-12">{input}{error}</div>'])
+<!-- <?php echo $form->field($model, 'report_id', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12">{input}{error}</div>'])
 	->textInput(['maxlength' => true])
 	->label($model->getAttributeLabel('report_id'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12']); ?> -->
-<?php echo $form->field($model,'report_id', ['template' => '{label}<div class="col-md-6 col-sm-6 col-xs-12">{input}{error}</div>'])
+<?php echo $form->field($model,'report_id', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12">{input}{error}</div>'])
 	->textInput()
 	->label($model->getAttributeLabel('report_id'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12'])->dropDownList( ArrayHelper::map (Reports::find()->All(),'report_id','reports'),
     ['prompt'=>'-- Choose a Report --']	) ?>
 
-<!-- <?php echo $form->field($model, 'user_id', ['template' => '{label}<div class="col-md-6 col-sm-6 col-xs-12">{input}{error}</div>'])
+<!-- <?php echo $form->field($model, 'user_id', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12">{input}{error}</div>'])
 	->textInput(['maxlength' => true])
 	->label($model->getAttributeLabel('user_id'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12']); ?> -->
 
-<?php echo $form->field($model, 'comment_text', ['template' => '{label}<div class="col-md-6 col-sm-6 col-xs-12">{input}{error}</div>'])
+<?php echo $form->field($model, 'comment_text', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12">{input}{error}</div>'])
 	->textarea(['rows'=>2,'rows'=>6])
 	->widget(Redactor::className(), ['clientOptions' => $redactorOptions])
 	->label($model->getAttributeLabel('comment_text'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12']); ?>
 
-<?php echo $form->field($model, 'publish', ['template' => '{label}<div class="col-md-6 col-sm-6 col-xs-12 checkbox">{input}{error}</div>'])
+<?php echo $form->field($model, 'publish', ['template' => '{label}<div class="col-md-9 col-sm-9 col-xs-12 checkbox">{input}{error}</div>'])
 	->checkbox(['label'=>''])
 	->label($model->getAttributeLabel('publish'), ['class'=>'control-label col-md-3 col-sm-3 col-xs-12']); ?>
 
