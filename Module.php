5fdb8cd41b3b1d8acfc387992ae71739d97ef88f
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2018-05-19 09:19:44 +0700

    commit

--- Module.php
+++ Module.php
@@ -1,16 +1,17 @@
 <?php
-namespace ommu\report;
-
 /**
  * report module definition class
  *
- * @copyright Copyright (c) 2017 ECC UGM (ecc.ft.ugm.ac.id)
- * @link http://ecc.ft.ugm.ac.id
  * @author Putra Sudaryanto <putra@sudaryanto.id>
- * @created date 15 September 2017, 15:06 WIB
  * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2017 ECC UGM (ecc.ft.ugm.ac.id)
+ * @created date 15 September 2017, 15:06 WIB
+ * @link http://ecc.ft.ugm.ac.id
  *
  */
+
+namespace ommu\report;
+
 class Module extends \app\components\Module
 {
 	public $layout = 'main';
