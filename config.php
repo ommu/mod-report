295750b8f76774c2fbf61276ad90c2e92968273a
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-01-28 18:19:04 +0700

    commit

--- config.php
+++ config.php
@@ -4,9 +4,9 @@
  *
  * @author Putra Sudaryanto <putra@sudaryanto.id>
  * @contact (+62)856-299-4114
- * @copyright Copyright (c) 2018 ECC UGM (ecc.ft.ugm.ac.id)
+ * @copyright Copyright (c) 2018 OMMU (www.ommu.co)
  * @created date 15 January 2018, 19:05 WIB
- * @link http://ecc.ft.ugm.ac.id
+ * @link https://github.com/ommu/mod-report
  *
  */
 
