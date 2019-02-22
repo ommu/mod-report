2eb912868eadf9b748132b542dc56959c716f383
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-02-22 15:19:40 +0700

    commit

--- views/admin/_form.php
+++ views/admin/_form.php
@@ -64,8 +64,8 @@ echo $form->field($model, 'cat_id')
 } ?>
 
 <div class="ln_solid"></div>
-<div class="form-group">
-	<div class="col-md-6 col-sm-9 col-xs-12 col-sm-offset-3">
+<div class="form-group row">
+	<div class="col-md-6 col-sm-9 col-xs-12 offset-sm-3">
 		<?php echo Html::submitButton($model->isNewRecord ? Yii::t('app', 'Create') : Yii::t('app', 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']); ?>
 	</div>
 </div>
