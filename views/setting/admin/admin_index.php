f42f6c2043d363c51346b665682989b61df7faed
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-05-04 09:15:09 +0700

    add alert flashMessage in setting

--- views/setting/admin/admin_index.php
+++ views/setting/admin/admin_index.php
@@ -91,10 +91,7 @@ echo GridView::widget([
 
 <div class="row">
 	<div class="col-md-12 col-sm-12 col-xs-12 col-12">
-		<?php if(Yii::$app->session->hasFlash('success'))
-			echo $this->flashMessage(Yii::$app->session->getFlash('success'));
-		else if(Yii::$app->session->hasFlash('error'))
-			echo $this->flashMessage(Yii::$app->session->getFlash('error'), 'danger');?>
+		<?php echo \app\components\widgets\Alert::widget(); ?>
 
 		<div class="x_panel">
 			<div class="x_title">
