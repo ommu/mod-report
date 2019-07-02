e32e93812ae5fe8b52c6d1a6bac81f57a9df00e7
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2019-07-02 16:28:27 +0700

    commit

--- views/setting/category/admin_manage.php
+++ views/setting/category/admin_manage.php
@@ -51,6 +51,21 @@ array_push($columnData, [
 		if($action == 'delete')
 			return Url::to(['setting/category/delete', 'id'=>$key]);
 	},
+	'buttons' => [
+		'view' => function ($url, $model, $key) {
+			return Html::a('<span class="glyphicon glyphicon-eye-open"></span>', $url, ['title' => Yii::t('app', 'Detail')]);
+		},
+		'update' => function ($url, $model, $key) {
+			return Html::a('<span class="glyphicon glyphicon-pencil"></span>', $url, ['title' => Yii::t('app', 'Update')]);
+		},
+		'delete' => function ($url, $model, $key) {
+			return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, [
+				'title' => Yii::t('app', 'Delete'),
+				'data-confirm' => Yii::t('app', 'Are you sure you want to delete this item?'),
+				'data-method'  => 'post',
+			]);
+		},
+	],
 	'template' => '{view} {update} {delete}',
 ]);
 
