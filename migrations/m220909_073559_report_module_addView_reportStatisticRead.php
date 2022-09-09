1cee5b3c3d080b9ce3b22f1432963a3640714881
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2022-09-09 08:15:13 +0700

    migrate: add all view reportRead and insertInTo reportGrid

---
+++ migrations/m220909_073559_report_module_addView_reportStatisticRead.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * m220909_073559_report_module_addView_reportStatisticRead
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 9 September 2022, 07:38 WIB
+ * @link https://github.com/ommu/mod-report
+ *
+ */
+
+use Yii;
+use yii\db\Schema;
+
+class m220909_073559_report_module_addView_reportStatisticRead extends \yii\db\Migration
+{
+	public function up()
+	{
+        $this->execute('DROP VIEW `_report_statistic_read`');
+
+        // alter view _report_statistic_read
+        $alterViewReportStatisticRead = <<< SQL
+CREATE VIEW `_report_statistic_read` AS 
+SELECT
+`a`.`report_id` AS `report_id`,
+COUNT(`a`.`id`) AS `reads`
+FROM `ommu_report_read` `a`
+GROUP BY `a`.`report_id`;
+SQL;
+        $this->execute($alterViewReportStatisticRead);
+	}
+
+	public function down()
+	{
+        $this->execute('DROP VIEW `_report_statistic_read`');
+    }
+}
---
+++ migrations/m220909_074213_report_module_updateView_report_addStatisticRead.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * m220909_074213_report_module_updateView_report_addStatisticRead
+ * 
+ * @author Putra Sudaryanto <putra@ommu.id>
+ * @contact (+62)856-299-4114
+ * @copyright Copyright (c) 2022 OMMU (www.ommu.id)
+ * @created date 9 September 2022, 07:43 WIB
+ * @link https://github.com/ommu/mod-report
+ *
+ */
+
+use Yii;
+use yii\db\Schema;
+
+class m220909_074213_report_module_updateView_report_addStatisticRead extends \yii\db\Migration
+{
+	public function up()
+	{
+        $this->execute('DROP VIEW `_reports`');
+
+        // alter view _reports
+        $alterViewReports = <<< SQL
+CREATE VIEW `_reports` AS 
+SELECT
+  `a`.`report_id`   AS `report_id`,
+  COUNT(`b`.`id`)   AS `histories`,
+  `c`.`resolved`    AS `resolved`,
+  `c`.`unresolved`  AS `unresolved`,
+  `c`.`statuses`    AS `statuses`,
+  `d`.`comments`    AS `comments`,
+  `d`.`comment_all` AS `comment_all`,
+  `e`.`users`       AS `users`,
+  `f`.`reads`       AS `reads`
+FROM (((((`ommu_reports` `a`
+       LEFT JOIN `ommu_report_history` `b`
+         ON (`a`.`report_id` = `b`.`report_id`))
+      LEFT JOIN `_report_statistic_status` `c`
+        ON (`a`.`report_id` = `c`.`report_id`))
+     LEFT JOIN `_report_statistic_comment` `d`
+       ON (`a`.`report_id` = `d`.`report_id`))
+    LEFT JOIN `_report_statistic_user` `e`
+      ON (`a`.`report_id` = `e`.`report_id`))
+   LEFT JOIN `_report_statistic_read` `f`
+     ON (`a`.`report_id` = `f`.`report_id`))
+GROUP BY `a`.`report_id`;
+SQL;
+        $this->execute($alterViewReports);
+	}
+
+	public function down()
+	{
+        $this->execute('DROP VIEW `_reports`');
+
+        // alter view _reports
+        $alterViewReports = <<< SQL
+CREATE VIEW `_reports` AS 
+select
+  `a`.`report_id`   AS `report_id`,
+  count(`b`.`id`)   AS `histories`,
+  `c`.`resolved`    AS `resolved`,
+  `c`.`unresolved`  AS `unresolved`,
+  `c`.`statuses`    AS `statuses`,
+  `d`.`comments`    AS `comments`,
+  `d`.`comment_all` AS `comment_all`,
+  `e`.`users`       AS `users`
+from ((((`ommu_reports` `a`
+      left join `ommu_report_history` `b`
+        on (`a`.`report_id` = `b`.`report_id`))
+     left join `_report_statistic_status` `c`
+       on (`a`.`report_id` = `c`.`report_id`))
+    left join `_report_statistic_comment` `d`
+      on (`a`.`report_id` = `d`.`report_id`))
+   left join `_report_statistic_user` `e`
+     on (`a`.`report_id` = `e`.`report_id`))
+group by `a`.`report_id`;
+SQL;
+        $this->execute($alterViewReports);
+    }
+}
